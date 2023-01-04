e870a9a146fbdfc5dd307e932031323fa0d1b6b3
fllantada <franciscollantada@gmail.com>
2023-01-04 17:01:10 -0300

    Populated database with moock data and provide a mongoDB instance at a Docker Container

---
+++ bd/initialize.sh
@@ -0,0 +1,20 @@
+if [ ! -d "./data" ]; then
+  # Crea la carpeta /data/db en el host
+  echo "Creando carpeta /data en el host"
+  mkdir data
+fi
+
+docker stop db_videos_container
+docker rm db_videos_container
+docker rmi db_videos_image
+echo "Termine el stop.sh"
+
+# Construye la imagen de Docker
+docker build -t db_videos_image .
+
+# Ejecuta un contenedor de Docker con la carpeta /data/db montada como volumen
+docker run -d -p 27017:27017 -v "$(pwd)/data:/data" --name db_videos_container db_videos_image
+echo "Termine el run.sh"
+
+#cargar los datos
+
---
+++ bd/script/index.js
@@ -0,0 +1,44 @@
+import { MongoClient } from "mongodb";
+import { faker } from "@faker-js/faker";
+
+async function start() {
+  const db = await connectMongo();
+  const randomVideos = createRandomVideos(100);
+  const videos = db.collection("videos");
+  await videos.insertMany(randomVideos);
+  //close conection
+  process.exit(0);
+}
+
+start();
+
+export async function connectMongo() {
+  const url = "mongodb://localhost:27017";
+  try {
+    const client = await MongoClient.connect(url, { useNewUrlParser: true });
+    console.log("Connected to MongoDB");
+
+    const db = client.db("videos");
+    return db;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function createRandomVideos(n) {
+  const videos = [];
+  for (let i = 0; i < n; i++) {
+    videos.push({
+      title: faker.lorem.words(),
+      author: faker.internet.userName(),
+      views: faker.random.numeric(),
+      youtube_id: faker.random.alpha(10),
+      thumbnail_url: faker.image.imageUrl(),
+      slug: faker.lorem.slug(),
+      active: faker.random.numeric() % 3 !== 0,
+      created_date: faker.date.past(),
+      popularity: faker.random.numeric(),
+    });
+  }
+  return videos;
+}
