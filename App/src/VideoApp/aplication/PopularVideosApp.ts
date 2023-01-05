fd2c11c929154315cf62a83743c336d46e4e38a2
fllantada <franciscollantada@gmail.com>
2023-01-04 21:32:28 -0300

    editLike method from VideoApp v1 edit popularity

--- App/src/VideoApp/aplication/PopularVideosApp.ts
+++ App/src/VideoApp/aplication/PopularVideosApp.ts
@@ -2,11 +2,21 @@ import { Video } from "../domain/entities/video";
 import { VideoRepository } from "../domain/video.repository";
 
 export default class PopularVideosApp {
+  //Si bien puede verse muy redundante todo esto lo hago pensando en la consigna de
+  //algo escalable
+
+  //logica para no buscar videos si ya tengo los videos populares
+  videos: Video[] = [];
+
+  private popularVideos: Video[] = [];
+
   constructor(private videoRepository: VideoRepository) {}
 
   async getPopularVideos(): Promise<Video[]> {
-    const videos = await this.videoRepository.getPopularVideos(5);
-    return videos;
+    if (this.popularVideos.length > 0) {
+      this.popularVideos = await this.videoRepository.getPopularVideos(5);
+    }
+    return this.popularVideos;
   }
 
   async getVideo(id: string): Promise<Video> {
@@ -14,8 +24,22 @@ export default class PopularVideosApp {
     return video;
   }
 
-  async addLike(id: string, popularity: number): Promise<Video> {
-    const video = await this.videoRepository.addLike(id, popularity);
-    return video;
+  async editLike(id: string, action: "like" | "dislike"): Promise<Video> {
+    const video = await this.getVideo(id);
+    let newPopularity = video.popularity;
+
+    if (action === "dislike") {
+      newPopularity = video.popularity - 1;
+    }
+
+    if (action === "like") {
+      newPopularity = video.popularity + 1;
+    }
+
+    const editedVideo = await this.videoRepository.editPopularity(
+      id,
+      newPopularity
+    );
+    return editedVideo;
   }
 }
--- App/src/VideoApp/domain/video.repository.ts
+++ App/src/VideoApp/domain/video.repository.ts
@@ -3,8 +3,8 @@ import { Video } from "./entities/video";
 export interface VideoRepository {
   getPopularVideos(qty: number): Promise<Video[]>; //get all videos
   getVideo(id: string): Promise<Video>; //get video by id
-  addLike(id: string, popularity: number): Promise<Video>;
+  editPopularity(id: string, newPopularity: number): Promise<Video>;
   getComments(videoId: string): Promise<Comment[]>;
-  addComent(videoId: string, text: string): Promise<Comment>;
+  addComment(videoId: string, text: string): Promise<Comment>;
   //change video popularity
 }
