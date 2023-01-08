605febcfe25ffd80195e7e11674bc71302b4382b
fllantada <franciscollantada@gmail.com>
2023-01-08 14:20:49 -0300

    Css added to NAV

--- Front/PopularVideoApp/components/Nav.tsx
+++ Front/PopularVideoApp/components/Nav.tsx
@@ -1,20 +1,43 @@
-import { AppBar, Input, Button } from "@mui/material";
+import { AppBar, Input, Button, Typography } from "@mui/material";
 import { useState } from "react";
 
 export const Nav: React.FC = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <AppBar position='relative' style={{ backgroundColor: "#E8E8E8" }}>
+    <AppBar
+      position='relative'
+      style={{
+        backgroundColor: "#E8E8E8",
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "right",
+      }}
+    >
       {isLoggedIn ? (
         <Button variant='contained' onClick={() => setIsLoggedIn(false)}>
           Cerrar sesión
         </Button>
       ) : (
         <>
-          <Input placeholder='Usuario' />
-          <Input placeholder='Contraseña' type='password' />
-          <Button variant='contained' onClick={() => setIsLoggedIn(true)}>
+          <Typography variant='h6' sx={{ margin: "0px 20px", color: "black" }}>
+            Ingrese email y contraseña para poder dar Like o comentar el video:
+          </Typography>
+          <Input
+            sx={{ minWidth: "400px", margin: "0px 20px" }}
+            placeholder='Email'
+          />
+          <Input
+            sx={{ minWidth: "200px", margin: "0px 20px" }}
+            placeholder='Contraseña'
+            type='password'
+          />
+          <Button
+            sx={{ minWidth: "200px", margin: "0px 20px" }}
+            variant='contained'
+            onClick={() => setIsLoggedIn(true)}
+          >
             Iniciar sesión
           </Button>
         </>
