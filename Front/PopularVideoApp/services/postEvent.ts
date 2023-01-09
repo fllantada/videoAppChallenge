569a17be618937a885b47c26dffb5a50987c94a2
fllantada <franciscollantada@gmail.com>
2023-01-09 20:56:59 -0300

    token added to post event

--- Front/PopularVideoApp/services/postEvent.ts
+++ Front/PopularVideoApp/services/postEvent.ts
@@ -1,7 +1,13 @@
 import axios from "axios";
+import { useUserStore } from "../store/userAuthUser.store";
 type validAction = "addLike" | "removeLike" | "addComment" | "removeComment";
 
 export default async function postEvent(videoId: string, action: validAction) {
+  const [token] = useUserStore((state) => [state.token]);
+
+  const header = "event-auth-token";
+  axios.defaults.headers.common[header] = token;
+
   const response = await axios.post(`http://localhost:8080/api/events`, {
     videoId: videoId,
     action: action,
